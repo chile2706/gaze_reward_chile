b395064d5e664a61b6569d33cd53cb58dc90e7fe
Chi Le <khanhchile@infra04-wg012.eduroam.wireless.umn.edu>
2025-05-10 10:15:29 -0500

    data path

--- rlhf_rw/reward_utils/dataset_proceser_reward.py
+++ rlhf_rw/reward_utils/dataset_proceser_reward.py
@@ -109,7 +109,7 @@ class DatasetProceserReward(DatasetProceser):
         max_length=None,
     ):
         if dataset_name == "our_data":
-            data = pd.read_csv("../../data/processed_stimuli.csv")
+            data = pd.read_csv("/users/0/le000422/gaze_reward_chile/data/processed_stimuli.csv")
         else:
             if split != "":
                 data = load_dataset(dataset_name, split=split)
