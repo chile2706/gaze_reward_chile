d5dd1ed2cc05c5bb7e92596b2ea639bd577c5009
Chi Le <khanhchile@ra-full-mfa.vpn.umn.edu>
2025-05-11 20:45:07 -0500

    update

--- data/process_data.py
+++ data/process_data.py
@@ -1,4 +1,19 @@
 import pandas as pd
 
 df = pd.read_csv("processed_stimuli.csv")
-df.head(10).to_csv("processed_stimuli_10.csv")
+df.head(100).to_csv("processed_stimuli_100.csv")
+# dict_keys(['fixations', 'attention_mask', 'mapped_fixations'])
+# import json
+
+# with open("fixation_records.jsonl") as f:
+#     records = [json.loads(line) for line in f]
+
+# rec = records[0]
+# m = len(rec['fixations'])
+# n = len(rec['fixations'][0])
+# print(m, n)
+# rec = records[1]
+
+# m = len(rec['fixations'])
+# n = len(rec['fixations'][0])
+# print(m, n)
